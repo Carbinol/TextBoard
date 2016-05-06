a63af935156c6b27b4643e6b24dc6fe33bc3c23b
Carbinol <carbinol.index@qq.com>
2016-05-07 00:10:03 +0800

    Update input.php

--- input.php
+++ input.php
@@ -7,6 +7,23 @@
 
 <center><h1>超简易留言板</h1></center>
 
+<center>
+<table border="0">
+<form action="input.php" method="post">
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" name="sub" value="管理员界面">
+		</td>
+	</tr>
+</form>
+</table>
+</center>
+<?php
+if( isset($_POST['sub'])){	
+header("Location:login.php");
+exit();
+}
+?>
 
 <!-- 输出已有内容 -->
 <?php
@@ -15,9 +32,7 @@ if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-
 mysql_select_db("msgs", $con);
-
 $result = mysql_query("SELECT * FROM msgv3 order by msgID asc");
 echo "<table border='1' width='100%'>";
 while($row = mysql_fetch_array($result))
@@ -39,7 +54,7 @@ if(isset($_COOKIE["username"]))    //若有cookie则可以编辑留言
 ?>
 <center>
 <table border="0">
-<form action="input.php" method="post">
+<form action="save.php" method="post">
 
 <caption><h1>添加留言</h1></caption>
 	<tr>
@@ -55,47 +70,8 @@ if(isset($_COOKIE["username"]))    //若有cookie则可以编辑留言
 </form>
 </table>
 </center>
-<!-- 表单内容写入数据库 -->
-
 <?php
-
-
-@$con = mysql_connect("localhost","root","");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-if( isset($_POST['submit'])){
-mysql_select_db("msgs", $con);
-
-$sql="INSERT INTO msgv3 (msgID, messagetext, ip, msgdate)
-VALUES
-(null,'$_POST[message]','{$_SERVER["REMOTE_ADDR"]}',now())";
-
-if (!mysql_query($sql,$con))
-  {
-  die('Error: ' . mysql_error());
-  }
-}
-mysql_close($con);
 }    //开头的if结尾
-?>
-<center>
-<table border="0">
-<form action="input.php" method="post">
-	<tr>
-		<td colspan="2" align="center">
-			<input type="submit" name="sub" value="管理员界面">
-		</td>
-	</tr>
-</form>
-</table>
-</center>
-<?php
-if( isset($_POST['sub'])){	
-header("Location:login.php");
-exit();
-}
 ?>
 
 </body>
